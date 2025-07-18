b0e19ebfd27149a95af8318e4c2b9743c67e3938
masthan6526 <147030557+masthan6526@users.noreply.github.com>
2025-07-18 15:42:29 +0530

    Delete backend.tf

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "example-rg22"
-    storage_account_name = "mytfstateaccount1"
-    container_name       = "tfstate"
-    key                  = "terraform.tfstate"
-  }
-}
